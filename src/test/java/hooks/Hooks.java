db7e3d78658df5d45c5861e5a21f6485df8e6e7f
Herlina <herlinaayu356@gmail.com>
2025-05-19 05:50:52 +0700

    resolve conflict

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.cucumber.java.Before;
 import io.cucumber.java.After;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Hooks {
 
@@ -12,12 +13,23 @@ public class Hooks {
 
     @Before
     public void setUp() {
+
         // Setup ChromeDriver otomatis sesuai versi Chrome
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
 
-    }
 
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new"); // headless mode (gunakan "--headless" jika versi lama)
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+
+    }
     @After
     public void tearDown() throws InterruptedException {
         if (driver != null) {
