f2064f8601983344c2c5f0e7167233914cc0ce0b
Herlina <herlinaayu356@gmail.com>
2025-05-19 06:19:49 +0700

    Remove target directory from Git

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -13,27 +13,23 @@ public class Hooks {
 
     @Before
     public void setUp() {
-
         // Setup ChromeDriver otomatis sesuai versi Chrome
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
 
-
-        WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new"); // headless mode (gunakan "--headless" jika versi lama)
+        options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--window-size=1920,1080");
 
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-
     }
+
     @After
     public void tearDown() throws InterruptedException {
         if (driver != null) {
-            Thread.sleep(5000); // delay 5 detik agar browser tidak langsung tertutup
+            Thread.sleep(5000);
             driver.quit();
         }
     }
